b164b03a7378b5ea9e83060d5de05feaa28b2969
CarlosVillaalba <104330333+CarlosVillaalba@users.noreply.github.com>
2025-04-13 17:30:33 -0400

    Add files via upload

---
+++ theMagic8ball.js
@@ -0,0 +1,19 @@
+const randomNumber = Math.floor(Math.random() * 8) + 1;
+
+if (randomNumber === 1) {
+  console.log('yes, definitely');
+} else if (randomNumber === 2) {
+  console.log('it is decidedly so');
+} else if (randomNumber === 3) {
+  console.log('Without a doubt');
+} else if (randomNumber === 4) {
+  console.log('Reply hazy, try again');
+} else if (randomNumber === 5) {
+  console.log('ask again later');
+} else if (randomNumber === 6) {
+  console.log('better not tell you now');
+} else if (randomNumber === 7) {
+  console.log('outlook not so good');
+} else {
+  console.log('very doubtful');
+}
